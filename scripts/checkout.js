e4a5d4e75a0b2d85bc6235afa6d10b6bc721b3f4
shantnuk <shantnu.kaushal22@stjohns.edu>
2025-04-29 02:36:34 -0400

    Add files via upload

---
+++ scripts/checkout.js
@@ -0,0 +1,5 @@
+import { renderOrderSummary } from "./Checkout/orderSummary.js";
+import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
+
+renderOrderSummary();
+renderPaymentSummary();
